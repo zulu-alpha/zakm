678f02e91fafbe201ebf01d7002a9ad79f762e2a
Adam Piskorski <adam@piskorski.me>
2019-03-18 17:05:45 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Zulu-Alpha Kit Manager
+
+Uses a [ZAKI](https://github.com/zulu-alpha/zaki) and kit specification YAML files to modify mission.sqm files to load out units and crates accordingly and generate am accompanying human readable document.
---
+++ tests/test_zakm.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+from yaml import load
+
+
+SPEC_PATH = Path(__file__).parent.parent / "zakm" / "kit_specification.yml"
+
+
+def test_test():
+    with open(SPEC_PATH, "r") as open_file:
+        data = load(open_file.read())
+        pass
